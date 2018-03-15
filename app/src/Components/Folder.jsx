b8b024fc4ca069e7fd6fed52228ebf3f8a15032f
Vankata <ivan.dimitrov293@gmail.com>
2018-03-16 00:22:13 +0200

    UI

--- app/src/Components/Folder.jsx
+++ app/src/Components/Folder.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 export default class Folder extends React.Component {
     render() {
         return (
-			<span>
-                Asd
-            </span>
+            <div>
+                <div className="sectioncontent">
+                    <i class="material-icons">keyboard_arrow_right</i>
+                    <p className="sectiontext">{this.props.text}</p>
+                </div>
+                {this.props.children}
+            </div>
         )
     }
 }
