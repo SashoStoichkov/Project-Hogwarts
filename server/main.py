5d2c85a5e67a4ff0d3144c886d383350fb2b0ba0
kristyanYochev <kris.yochev@gmail.com>
2018-03-15 23:37:34 +0200

    asd

--- server/main.py
+++ server/main.py
@@ -10,6 +10,7 @@ from passlib.hash import sha256_crypt as sha256
 from functools import wraps
 
 app = Flask(__name__)
+CORS(app)
 
 dict_c, c, conn = None, None, None
 
@@ -51,6 +52,13 @@ def login():
         return jsonify(code='3', error="Invalid password")
     
     return jsonify(code="1")
+
+@app.route('/register', methods=["POST"])
+@db
+def register():
+    uname = request.form['uname']
+    email = request.form['email']
+    
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
