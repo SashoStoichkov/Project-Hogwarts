d2148e6a92e077c0f2779da45a92ea4556549585
georgikorchakov <georgikorchakov@gmail.com>
2018-03-16 08:36:48 +0200

    ddd

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import Textfield from './Components/Textfield.jsx'
+import LoginRegister from './Pages/LoginRegister.jsx'
 
 export default class App extends React.Component {
     render() {
         return (
-            <Textfield holder="Asd"/>
+            <LoginRegister />
         );
     }
 }
