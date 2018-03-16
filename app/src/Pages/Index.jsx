2a37e5e682e7ac91018995075e348909c01eebbc
Alexander Stoichkov <sashostoichkov@gmail.com>
2018-03-16 09:48:37 +0200

    added navbar

--- app/src/Pages/Index.jsx
+++ app/src/Pages/Index.jsx
@@ -4,20 +4,29 @@ import Textfield from '../Components/Textfield.jsx'
 import Section from '../Components/Section.jsx'
 import Folder from '../Components/Folder.jsx'
 import File from '../Components/File.jsx'
+import NavBar from '../Components/NavBar.jsx'
 
 export default class Index extends React.Component {
     render() {
         return (
             <div>
-                <div id="leftside">
+                
+                <NavBar>
                     <img src="https://static.interestingengineering.com/images/import/2016/09/BLUE-SKY.jpg" id="logo"  alt="logo" />
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>                    
+                </NavBar>
+                <div id="leftside">
+                    
                     <Section text="Project Hogwarts">
                         <Folder text="app">
                             <File text="index.html"/>
                         </Folder>
                     </Section>
                 </div>
-
+                
             </div>
         );
     }
