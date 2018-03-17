5bac2aab4ebdb1d1a78b0a9bf8131bfae1fd9669
georgi <georgi@localhost.localdomain>
2018-03-17 22:14:27 +0200

    Added context menu (remains functionality)

--- app/src/Components/File.jsx
+++ app/src/Components/File.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import fileIcons from 'file-icons-js';
 
+import { ContextMenu, Item, Separator, Submenu, ContextMenuProvider } from 'react-contexify';
+import 'react-contexify/dist/ReactContexify.min.css';
+
+const FileMenu = () => (
+    <ContextMenu id='file_id'>
+       <Item>Rename</Item>
+       <Item>Delete</Item>
+    </ContextMenu>
+);
+
 export default class File extends React.Component {
     constructor(props) {
         super(props)
@@ -15,10 +25,15 @@ export default class File extends React.Component {
     
     render() {
         return (
-            <div className="animated fadeInLeft sectioncontent space" onClick={this.onClick} >
-                <i style={{marginRight : '7px'}} className={fileIcons.getClassWithColor(this.props.text)}></i>
-                <p className="sectiontext">{this.props.text}</p>
-            </div>
+            <div>   
+                <ContextMenuProvider id="file_id">         
+                    <div className="animated fadeInLeft sectioncontent space" onClick={this.onClick} >
+                        <i style={{marginRight : '7px'}} className={fileIcons.getClassWithColor(this.props.text)}></i>
+                        <p className="sectiontext">{this.props.text}</p>
+                    </div>
+                </ContextMenuProvider>
+                <FileMenu />
+            </div>  
         )
     }
 }
