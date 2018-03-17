7d976d14c6185c2126c53f255825c0c7ccb251ce
kristyanYochev <kris.yochev@gmail.com>
2018-03-17 16:27:11 +0200

    fixed a small bug

--- server/main.py
+++ server/main.py
@@ -59,7 +59,7 @@ def get_folder_structure(dir_name):
 
 @app.route('/')
 def index():
-    return render_template('test.html')
+    return render_template('index.html')
 
 @app.route('/login', methods=['POST'])
 @db
